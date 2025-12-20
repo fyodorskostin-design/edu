c7fce8fc9934dffccdfdb651402ca76d2568a3f4
fyodorskostin-design <fyodor.s.kostin@gmail.com>
2025-12-20 10:30:37 +0000

    20.12

---
+++ 1.py
@@ -0,0 +1,10 @@
+a = '10'
+b = '10'
+
+a = int(a)
+b = int(b)
+
+a = str(a)
+b = str(b)
+
+print(a + b)
---
+++ 2.py
@@ -0,0 +1,35 @@
+a = 2
+b = 3
+c = b
+b = a
+a = c
+print(a, b)
+
+
+a = 2
+b = 3
+a = a + b
+b = a - b
+a = a - b
+print(a, b)
+
+
+a = 2
+b = 3
+c = b
+d = a
+a = c
+b = d
+print(a, b)
+
+
+a, b = 2, 3
+a, b = b, a
+print(a, b)
+
+
+a = int(input())
+print(a+a)
+
+
+
